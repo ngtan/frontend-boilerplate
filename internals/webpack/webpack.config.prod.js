d2cb2cf5ba9ce9c18ed34d8634481b53e9670061
ngtan <tannd1993@gmail.com>
2018-07-02 10:03:39 +0700

    progressive web app

--- internals/webpack/webpack.config.prod.js
+++ internals/webpack/webpack.config.prod.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = require('./webpack.config.base')({
   mode: 'production',
@@ -17,5 +18,17 @@ module.exports = require('./webpack.config.base')({
     new CleanWebpackPlugin([
       path.resolve(process.cwd(), 'public'),
     ], { allowExternal: true }),
+
+    new WebpackPwaManifest({
+      name: 'frontend boilerplate',
+      short_name: 'fe boilerplate',
+      description: 'Make the development process as fast as possible',
+      icons: [
+        {
+          src: path.resolve('app/images/icon-512x512.png'),
+          sizes: [72, 96, 120, 128, 144, 152, 167, 180, 192, 384, 512],
+        },
+      ],
+    })
   ],
 });
