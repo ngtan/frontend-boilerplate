4e0cca36768256f88fc48b37943d85f5c831172d
Tan Nguyen <tannd1993@gmail.com>
2018-10-26 10:54:03 +0700

    remove bower_components

--- internals/webpack/webpack.config.base.js
+++ internals/webpack/webpack.config.base.js
@@ -110,7 +110,7 @@ module.exports = options => ({
       },
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
